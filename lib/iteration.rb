7dc2a06a3969321079a8e44cc1ab34d9073616a5
Joe Gelay <joegelay@gmail.com>
2020-01-13 19:44:14 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -62,7 +62,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if ((src[row_index][element_index] % 2 == 0) && (src[row_index][element_index + 1] % 2 == 0))
-      even_pair_totals.push(src[row_index][element_index] + src[row_index][element_index + 1])
+      even_pairs_sum += src[row_index][element_index] + src[row_index][element_index + 1]
     end
     element_index += 2
   end
