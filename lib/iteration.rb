2acac995e6980af8debdcc71b03b65de1e35a32f
Joe Gelay <joegelay@gmail.com>
2020-01-13 18:02:15 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -29,7 +29,6 @@ while row_index < src.count do
   element_index = 0
   inner_results = []
   while element_index < src[row_index].count do
-    if src[row_index][element_index][0] == "P"
       inner_results << src[row_index][element_index]
     end
     element_index += 1
